65bac1436dc230f8e4a43d494b0763b15ca04971
Michael <michaeltkboisvert@gmail.com>
2013-03-13 15:10:32 +0000

    Tested and added division methods

---
+++ Calculator.Tests/DivisionFixtures/When_dividing_numbers.cs
@@ -0,0 +1,22 @@
+﻿using CalculatorApp;
+using NUnit.Framework;
+
+namespace Calculator.Tests.DivisionFixtures
+{
+    [TestFixture]
+    class When_dividing_numbers
+    {
+        [Test]
+        public void Should_return_a_proper_quotient()
+        {
+
+            Divider divider = new Divider();
+
+            int result = divider.Calculate(9, 3);
+
+            Assert.AreEqual(3, result);
+
+        }
+
+    }
+}
