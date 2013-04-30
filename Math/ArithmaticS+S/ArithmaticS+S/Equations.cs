fda04c6e41620838dd6e9175315fffaae39538b1
Michael <tkboisvert@gmail.com>
2013-04-30 16:44:11 +0100

    Quadratic Formula.csproj is functional. No tests. ArithmaticS+S.csproj does NOT work. No tests.

---
+++ Math/ArithmaticS+S/ArithmaticS+S/Equations.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArithmaticS_S
+{
+    class Equations
+    {
+        public static int AddEntireSequence(int length_N, int startingDigit_B, string arrayEquation)
+        {
+            string[] equation = new string[10];
+            int i = 0;
+            foreach (char c in arrayEquation)
+            {
+                
+                if (char.IsDigit(c))
+                {
+                    string s = Convert.ToString(c);
+                    equation[i] = s;
+                }
+                if (c == 'b')
+                {
+                    string startingDigit = Convert.ToString(startingDigit_B);
+                    equation[i] = startingDigit;
+                }
+                if (c == '*')
+                {
+                    equation[i] = "*";
+                }
+                if (c == '/')
+                {
+                    equation[i] = "/";
+                }
+                if (c == '+')
+                {
+                    equation[i] = "+";
+                }
+                if (c == '-')
+                {
+                    equation[i] = "-";
+                }
+
+
+                i++;
+            }
+
+            int a1 = 0;
+            return i;
+        }
+    }
+}
