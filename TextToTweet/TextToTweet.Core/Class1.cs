d6c7e32b3386b21c8f4a9c5fb1f54ad2ed300ff4
Michael <tkboisvert@gmail.com>
2013-08-22 21:14:37 +0100

    added a few things

--- TextToTweet/TextToTweet.Core/Class1.cs
+++ TextToTweet/TextToTweet.Core/Class1.cs
@@ -2,36 +2,17 @@
 using System.IO;
 using System.Net;
 
+using TweetSharp;
+using Hammock;
+using Newtonsoft.Json;
+
 namespace TextToTweet.Core
 {
     public class Class1
     {
         public static void PostTweet(string username, string password, string tweet)
-
         {
-            try {
-    	        // encode the username/password
-    	        string user = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(username + ":" + password));
-    	        // determine what we want to upload as a status
-    	        byte[] bytes = System.Text.Encoding.ASCII.GetBytes("status=" + tweet);
-    	        // connect with the update page
-    	        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://twitter.com/statuses/update.xml");
-    	        // set the method to POST
-    	        request.Method="POST";
-    	        request.ServicePoint.Expect100Continue = false; // thanks to argodev for this recent change!
-    	        // set the authorisation levels
-    	        request.Headers.Add("Authorization", "Basic " + user);
-    	        request.ContentType="application/x-www-form-urlencoded";
-    	        // set the length of the content
-    	        request.ContentLength = bytes.Length;
-
-    	        // set up the stream
-    	        Stream reqStream = request.GetRequestStream();
-    	        // write to the stream
-    	        reqStream.Write(bytes, 0, bytes.Length);
-    	        // close the stream
-    	        reqStream.Close();
-            } catch (Exception ex) {/* DO NOTHING */}
+            PostTweet(username, password, tweet);
         }
     }
 }
