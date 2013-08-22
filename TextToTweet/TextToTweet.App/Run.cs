2e311f659d61ea67bb4a1650c019fa73ed62f64c
Michael <tkboisvert@gmail.com>
2013-08-22 19:01:51 +0100

    Added TextToTweet

---
+++ TextToTweet/TextToTweet.App/Run.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TextToTweet.Core;
+
+
+namespace TextToTweet.App
+{
+    public class Run
+    {
+        private static void Main(string[] args)
+        {
+            Class1.PostTweet("theboysenberry", "airkid123", "Test- *Sent from Text-To-Tweet");
+        }
+    }
+}
